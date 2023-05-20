65da2a014de279ff1578beb9730c1616a1aa4114
Romain Monteil <monteil.romain@gmail.com>
2023-05-20 11:56:33 +0200

    :construction_worker: Update Fcm test

--- tests/FcmTest.php
+++ tests/FcmTest.php
@@ -6,6 +6,8 @@ namespace Kerox\Fcm\Tests;
 
 use Kerox\Fcm\Api\Send;
 use Kerox\Fcm\Fcm;
+use Kerox\Fcm\Model\Message;
+use Kerox\Fcm\Model\Target\Topic;
 use PHPUnit\Framework\TestCase;
 
 final class FcmTest extends TestCase
@@ -14,7 +16,7 @@ final class FcmTest extends TestCase
 
     protected function setUp(): void
     {
-        $this->fcm = new Fcm('4321dcba', 'abcd1234');
+        $this->fcm = new Fcm(getenv('FCM_OAUTH_TOKEN'), getenv('FCM_PROJECT_ID'));
     }
 
     protected function tearDown(): void
@@ -24,6 +26,20 @@ final class FcmTest extends TestCase
 
     public function testItCanGetAnInstanceOfSendApi(): void
     {
-        self::assertInstanceOf(Send::class, $this->fcm->send());
+        $sendApi = $this->fcm->send();
+
+        self::assertInstanceOf(Send::class, $sendApi);
+
+        $response = $sendApi->message(new Message(
+            notification: 'Breaking News',
+            target: new Topic('TopicA'),
+            data: [
+                'story_id' => 'story_12345',
+            ],
+        ));
+
+        self::assertSame(200, $response->getStatusCode());
+        self::assertStringContainsString('projects/'.getenv('FCM_PROJECT_ID').'/messages/', $response->getBody()->getContents());
+        self::assertArrayHasKey('content-type', $response->getHeaders());
     }
 }
